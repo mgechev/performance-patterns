996b3405cb99d0479faaee0976811c11765feb4b
mgechev <mgechev@gmail.com>
2021-11-09 11:51:36 -0800

    Run the chart outside the zone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 
 import * as Plotly from 'plotly.js-dist-min';
 
@@ -39,7 +39,7 @@ export class AppComponent implements OnInit {
   rndList: EmployeeData[] = Rnd;
   label = '';
 
-  constructor(private generator: ListGenerator) {}
+  constructor(private generator: ListGenerator, private zone: NgZone) {}
 
   ngOnInit() {
     const data: [{x: string[], y: number[], type: 'bar'}] = [{
@@ -62,7 +62,9 @@ export class AppComponent implements OnInit {
       data[0].y.push(entity[1]);
     }
 
-    Plotly.newPlot('chart', data as any);
+    this.zone.runOutsideAngular(() => {
+      Plotly.newPlot('chart', data as any);
+    });
   }
 
   add(list: EmployeeData[], name: string) {
